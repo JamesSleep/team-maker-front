824c2f21f80480f1bf047024b7853dac1ebb4890
JamesSleep <jamessleep@donghaui-MacBookPro.local>
2021-10-22 13:33:47 +0900

    라우터 수정

--- src/router/StackRouter.js
+++ src/router/StackRouter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginPage from "../Screen/MainScreen/LoginPage";
+import SignUp from "../Screen/MainScreen/SignUpPage";
 
 const Stack = createNativeStackNavigator();
 
@@ -8,6 +9,7 @@ const StackRouter = () => {
   return (
     <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Login" component={LoginPage} />
+      <Stack.Screen name="SignUp" component={SignUp} />
     </Stack.Navigator>
   )
 } 
