3df2614242701106056c23db8c20c9614ce2f1f9
김동하 <supercurve2256@gmail.com>
2021-10-22 11:30:23 +0900

    앱 공통 색상

---
+++ src/Common/theme.js
@@ -0,0 +1,6 @@
+import { Dimensions } from "react-native";
+
+export const _WIDTH = Dimensions.get("window").width;
+export const _HEIGHT = Dimensions.get("window").height;
+export const ButtonColor = "#E42346";
+export const TextColor = "#e5e9f2";
