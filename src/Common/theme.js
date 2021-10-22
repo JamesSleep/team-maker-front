4677c3773a7f34e86c7d1f7468947b563bd35521
김동하 <supercurve2256@gmail.com>
2021-10-22 13:32:08 +0900

    앱 테마 색상 추가

--- src/Common/theme.js
+++ src/Common/theme.js
@@ -4,3 +4,5 @@ export const _WIDTH = Dimensions.get("window").width;
 export const _HEIGHT = Dimensions.get("window").height;
 export const ButtonColor = "#E42346";
 export const TextColor = "#e5e9f2";
+export const MainColor = "#1D1F2B";
+export const SubColor = "#2D2D39";
