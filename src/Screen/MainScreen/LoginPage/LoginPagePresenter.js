121cc5640d4f0e279b526b7205839abc3edbfd6f
JamesSleep <jamessleep@donghaui-MacBookPro.local>
2021-10-22 11:28:52 +0900

    로그인 페이지 디자인

--- src/Screen/MainScreen/LoginPage/LoginPagePresenter.js
+++ src/Screen/MainScreen/LoginPage/LoginPagePresenter.js
@@ -1,5 +1,10 @@
 import React from "react";
 import styled from "styled-components/native";
+import TitleView from "../../../Components/Login/TitleView";
+import InputContainer from "../../../Components/Login/InputContainer";
+import OptionContainer from "../../../Components/Login/OptionContainer";
+import { _WIDTH, ButtonColor, TextColor } from "../../../Common/theme";
+import { TouchableWithoutFeedback, Keyboard, TouchableOpacity } from "react-native";
 
 const Container = styled.View`
   width: 100%;
@@ -7,11 +12,17 @@ const Container = styled.View`
 `;
 
 const Cover = styled.View`
+  flex: 1;
   position: absolute;
   width: 100%;
   height: 100%;
   background-color: rgba(0,0,0,0.7);
   z-index: 1;
+  justify-content: flex-end;
+  align-items: center;
+  padding-left: ${_WIDTH * 0.1}px;
+  padding-right: ${_WIDTH * 0.1}px;
+  padding-bottom: ${_WIDTH * 0.25}px;
 `;
 
 const IamgeView = styled.View`
@@ -26,11 +37,54 @@ const Image = styled.Image`
   height: 100%;
 `;
 
+const ButtonContainer = styled.View`
+  width: 100%;
+  height: 70px;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 15px;
+`;
+
+const Button = styled.View`
+  width: ${_WIDTH * 0.8}px;
+  height: ${_WIDTH * 0.13}px;
+  background-color: ${props => props.buttonColor};
+  border-radius: 30px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonText = styled.Text`
+  color: ${props=>props.textColor};
+  font-size: ${_WIDTH * 0.05}px;
+  font-weight: bold;
+`;
+
 export default () => (
-  <Container>
-    <IamgeView>
-      <Image source={require("../../../images/loginback.png")} />
-    </IamgeView>
-    <Cover></Cover>
-  </Container>
+  <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <Container>
+      <IamgeView>
+        <Image source={require("../../../images/loginback.png")} />
+      </IamgeView>
+      <Cover>
+        <TitleView />
+        <InputContainer />
+        <ButtonContainer>
+          <TouchableOpacity>
+            <Button buttonColor={ButtonColor}>
+              <ButtonText textColor={TextColor}>로그인</ButtonText>
+            </Button>
+          </TouchableOpacity>
+        </ButtonContainer>
+        <ButtonContainer>
+          <TouchableOpacity>
+            <Button buttonColor={TextColor}>
+              <ButtonText textColor={"#2f3640"}>회원가입</ButtonText>
+            </Button>
+          </TouchableOpacity>
+        </ButtonContainer>
+        <OptionContainer />
+      </Cover>
+    </Container>
+  </TouchableWithoutFeedback>
 )
