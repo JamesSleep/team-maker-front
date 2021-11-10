8dd442171219094dcc9edc038ce621059d1c5024
JamesSleep <jamessleep@donghaui-MacBookPro.local>
2021-11-10 17:12:04 +0900

    길드 및 팀 가입화면 생성

---
+++ src/Screen/MainScreen/SelectOption/SelectOptionContainer.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import SelectOptionPresenter from "./SelectOptionPresenter";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { postMessage } from "../../../Util/postMessage";
+import { userAPI } from "../../../Common/api";
+import { Alert } from "react-native";
+
+export default ({ navigation }) => {
+  const [data, setData] = useState("");
+
+  const postData = async () => {
+    if (data.length < 1) {
+      postMessage("팀명을 입력해주세요");
+      return;
+    }
+    const result = await userAPI.findGuild(data);
+    if (!result[0]) {
+      Alert.alert(
+        "",
+        `${data} 팀이 존재하지않습니다\n새로운 팀을 생성하시겠습니까?`,
+        [
+          { text: "예", onPress: () => alertHandler()},
+          { text: "아니오", style: "cancel" }
+        ]
+      );
+    } else alertHandler();
+  }
+
+  const alertHandler = async () => {
+    const userInfo = JSON.parse(await AsyncStorage.getItem("userInfo"));
+    const userData = JSON.stringify({
+      "email": userInfo.email,
+      "password": userInfo.password,
+      "guild": data
+    });
+    const result = await userAPI.modify(userData);
+    if (result[0]) {
+      navigation.reset({
+        index: 0,
+        routes: [{ name: 'Home' }],
+      });
+    } else {
+      console.log(result);
+    }
+  }
+
+  return (
+    <SelectOptionPresenter 
+      postData={postData}
+      data={data}
+      setData={setData}
+    />
+  )
+}
---
+++ src/Screen/MainScreen/SelectOption/SelectOptionPresenter.js
@@ -0,0 +1,84 @@
+import React from "react";
+import styled from "styled-components/native";
+import { _WIDTH, MainColor, InputColor, TextColor, ButtonColor } from "../../../Common/theme";
+import { TouchableOpacity } from "react-native";
+
+const Container = styled.View`
+  width: 100%;
+  height: 100%;
+  padding-top: 50px;
+  padding-left: ${_WIDTH * 0.05}px;
+  padding-right: ${_WIDTH * 0.05}px;
+  background-color: ${MainColor};
+  justify-content: center;
+  align-items: center;
+`;
+
+const Text = styled.Text`
+  color: ${TextColor};
+  font-size: ${_WIDTH * 0.05}px;
+  font-weight: bold;
+`;
+
+const InputView = styled.View`
+  width: 100%;
+  height: ${_WIDTH * 0.13}px;
+  margin-top: 20px;
+  border-radius: 15px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: ${InputColor};
+`;
+
+const TextInput = styled.TextInput`
+  width: 100%;
+  height: ${_WIDTH * 0.125}px;
+  font-size: ${_WIDTH * 0.05}px;
+`
+
+const ButtonContainer = styled.View`
+  width: 100%;
+  height: 70px;
+  justify-content: center;
+  align-items: center;
+  margin-top: 15px;
+`;
+
+const Button = styled.View`
+  width: ${_WIDTH * 0.9}px;
+  height: ${_WIDTH * 0.13}px;
+  background-color: ${props => props.buttonColor};
+  border-radius: 30px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonText = styled.Text`
+  color: ${props=>props.textColor};
+  font-size: ${_WIDTH * 0.05}px;
+  font-weight: bold;
+`;
+
+export default ({ postData, data, setData }) => (
+  <Container>
+    <Text>입장하실 팀명의 이름을 입력해주세요</Text>
+    <InputView>
+      <TextInput 
+        placeholder="팀명"
+        placeholderTextColor="rgba(245,246,250, 0.5)"
+        color={TextColor}
+        textAlign={'center'}
+        value={data}
+        onChangeText={text => setData(text)}
+      />
+    </InputView>
+    <ButtonContainer>
+      <TouchableOpacity onPress={() => postData()}>
+        <Button buttonColor={ButtonColor}>
+          <ButtonText textColor={TextColor}>입장하기</ButtonText>
+        </Button>
+      </TouchableOpacity>
+    </ButtonContainer>
+  </Container>
+)
