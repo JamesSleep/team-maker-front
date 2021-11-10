4b2ea30188152a39b20606ebe05d250308114373
김동하 <supercurve2256@gmail.com>
2021-11-10 17:10:08 +0900

    색상 수정 및 추가

--- src/Common/theme.js
+++ src/Common/theme.js
@@ -6,3 +6,4 @@ export const ButtonColor = "#E42346";
 export const TextColor = "#e5e9f2";
 export const MainColor = "#1D1F2B";
 export const SubColor = "#2D2D39";
+export const InputColor = "#2D2D39";
