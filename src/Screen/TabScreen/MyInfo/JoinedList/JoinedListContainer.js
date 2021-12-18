5d7d3cef571dfb3c5d7bd6c06614e1cfe46f9b1a
JamesSleep <jamessleep@donghaui-MacBookPro.local>
2021-12-18 10:05:07 +0900

    레이드 참가 정보페이지 추가

--- src/Screen/TabScreen/MyInfo/JoinedList/JoinedListContainer.js
+++ src/Screen/TabScreen/MyInfo/JoinedList/JoinedListContainer.js
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import JoinedListPresenter from "./JoinedListPresenter";
+import { teamAPI, userAPI } from "../../../../Common/api";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default () => {
+export default ({ navigation }) => {
+  const [filter, setFilter] = useState(true);
+  const [data, setData] = useState([]);
+  const [userInfo, setUserInfo] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getData();
+  }, [filter]);
+
+  const getData = async () => {
+    const userInfo = JSON.parse(await AsyncStorage.getItem("userInfo"));
+    const searchUser = await userAPI.getOneUser(userInfo.email);
+    setUserInfo(searchUser[1]);
+
+    if (!searchUser[0]) return;
+
+    const searchTeam = await teamAPI.getAllTeamByMan(searchUser[1].index);
+    let array = searchTeam[1];
+
+    const today = new Date();
+    const timestamp = today.getTime();
+
+    if (filter) {
+      array = array.filter(item => item.timestamp >= timestamp);
+      setData(array);
+    } else {
+      array = array.filter(item => item.timestamp < timestamp);
+      setData(array);
+    }
+    setLoading(false);
+  }
+
   return (
-    <JoinedListPresenter />
+    <JoinedListPresenter 
+      navigation={navigation}
+      filter={filter}
+      setFilter={setFilter}
+      data={data}
+      userInfo={userInfo}
+      loading={loading}
+      setLoading={setLoading}
+    />
   )
 }
