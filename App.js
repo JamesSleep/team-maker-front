0a411b462126c64288b0f88d8dc149d80d1796aa
김동하 <supercurve2256@gmail.com>
2022-08-24 10:17:45 +0900

    typescript transform

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
-
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
-import { NavigationContainer } from "@react-navigation/native";
+import {NavigationContainer} from '@react-navigation/native';
 import StackRouter from './src/router/StackRouter';
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { StatusBar, Platform } from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {StatusBar, Platform} from 'react-native';
 
 const App = () => {
   useEffect(() => {
@@ -15,16 +14,11 @@ const App = () => {
 
   return (
     <SafeAreaProvider>
-      {
-        Platform.OS === "android" ?
-        (<StatusBar 
-          backgroundColor="transparent"
-          translucent={true}
-        />) :
-        (<StatusBar 
-          barStyle="light-content"
-        />)
-      }
+      {Platform.OS === 'android' ? (
+        <StatusBar backgroundColor="transparent" translucent={true} />
+      ) : (
+        <StatusBar barStyle="light-content" />
+      )}
       <NavigationContainer>
         <StackRouter />
       </NavigationContainer>
